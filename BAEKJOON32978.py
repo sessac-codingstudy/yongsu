c78b23ff6e9463bcddc28e0f0181bf2e3f1d26c0
Fnels <62380549+Fnels@users.noreply.github.com>
2025-12-16 09:10:20 +0900

    Implement input handling and data processing
    Added functions to handle input and process data.

--- BAEKJOON32978.py
+++ BAEKJOON32978.py
@@ -1,1 +1,18 @@
 #https://www.acmicpc.net/problem/32978
+
+def get_input():
+    a = input()
+    b = input().split()
+    c = input().split()
+    return a, b, c
+
+def marneul():
+    a, b, c = get_input()
+    d = dict.fromkeys(b, 0)
+    for x in c:
+        d[x] = 1
+    for key, value in d.items():
+        if value == 0:
+            print(key)
+
+marneul()
